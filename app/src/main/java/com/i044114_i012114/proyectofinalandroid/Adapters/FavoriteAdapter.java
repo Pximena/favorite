3431afdb2b40d18cae03b37156e2b7d1c18e59f8
ximena <xime.huertas@rocketmail.com>
2017-11-27 17:42:36 -0500

    Initial commit

---
+++ app/src/main/java/com/i044114_i012114/proyectofinalandroid/Adapters/FavoriteAdapter.java
@@ -0,0 +1,67 @@
+package com.i044114_i012114.proyectofinalandroid.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.i044114_i012114.proyectofinalandroid.Models.Favorite;
+import com.i044114_i012114.proyectofinalandroid.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by equipo14 on 27/11/17.
+ */
+
+public class FavoriteAdapter extends  RecyclerView.Adapter<FavoriteAdapter.ViewHolder> {
+
+    List<Favorite> favoriteList = new ArrayList<>();
+    Context context;
+
+    public FavoriteAdapter(List<Favorite> favoriteList, Context context) {
+        this.favoriteList = favoriteList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_favorite, parent, false);
+        ViewHolder viewHolder = new ViewHolder(item);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.textViewid_fav.setText(String.valueOf(favoriteList.get(position).getId_fav()));
+        holder.textViewid_user.setText(String.valueOf(favoriteList.get(position).getId_user()));
+        holder.textViewid_prod.setText(String.valueOf(favoriteList.get(position).getId_prod()));
+        //Picasso.with(context).load(contactList.get(position).getUrl()).into(holder.imageView);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return favoriteList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewid_fav;
+        TextView textViewid_prod;
+        TextView textViewid_user;
+
+
+        public ViewHolder(View item) {
+            super(item);
+
+            textViewid_fav = (TextView) item.findViewById(R.id.id_tv_item_name_fav);
+            textViewid_prod = (TextView) item.findViewById(R.id.id_tv_item_des_fav);
+            textViewid_user = (TextView) item.findViewById(R.id.id_tv_item_can_fav);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/i044114_i012114/proyectofinalandroid/Models/Favorite.java
@@ -0,0 +1,37 @@
+package com.i044114_i012114.proyectofinalandroid.Models;
+
+/**
+ * Created by equipo14 on 27/11/17.
+ */
+
+public class Favorite {
+
+
+    private int id_fav;
+    private int id_prod;
+    private int id_user;
+
+    public int getId_fav() {
+        return id_fav;
+    }
+
+    public void setId_fav(int id_fav) {
+        this.id_fav = id_fav;
+    }
+
+    public int getId_prod() {
+        return id_prod;
+    }
+
+    public void setId_prod(int id_prod) {
+        this.id_prod = id_prod;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+}
